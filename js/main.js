2b6e4c213a1d18240300cd2bfcc0e4367c004d93
Roberto <r.attanasio90@gmail.com>
2021-04-06 14:49:59 +0200

    Conversione rating con stelline

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ var app = new Vue({
                 axios
                     .get('https://api.themoviedb.org/3/search/movie?api_key=7fae8a8bc6eec8f4928cbdfba92b7045&language=it-IT&query=' + inputSearch)
                     .then((movieSearchList => {
-                        console.log(movieSearchList);
+                        movieSearchList.data.results.forEach(movie => {
+                            movie.vote_average = Math.ceil(((movie.vote_average * 5) / 10));
+                        });
                         this.movieList = movieSearchList.data.results;
                     }))
             } else {
