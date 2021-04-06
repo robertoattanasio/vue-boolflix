a074edd4217f8cc2474ece2bf3d9ad11d7902f61
Roberto <r.attanasio90@gmail.com>
2021-04-06 12:06:54 +0200

    Milestone 1 con grafica molto brutta completata

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@ var app = new Vue({
     el: '#boolflix',
     data: {
         inputSearch: '',
+        movieList: []
     },
+
     methods: {
         sendSearch: function(inputSearch) {
-            axios
-                .get('https://api.themoviedb.org/3/search/movie?api_key=7fae8a8bc6eec8f4928cbdfba92b7045&language=it-IT&query=' + inputSearch)
-                .then((movie => {
-                    console.log(movie);
-                }))
+            if (inputSearch != '') {
+                axios
+                    .get('https://api.themoviedb.org/3/search/movie?api_key=7fae8a8bc6eec8f4928cbdfba92b7045&language=it-IT&query=' + inputSearch)
+                    .then((movieSearchList => {
+                        console.log(movieSearchList);
+                        this.movieList = movieSearchList.data.results;
+                    }))
+            } else {
+                this.movieList = [];
+            }
         }
     }
 });
