3431cfad945a65f362bfdc668b1974543e5d2c25
Roberto <r.attanasio90@gmail.com>
2021-04-07 12:09:02 +0200

    Milestone 2 e 3 completate, inizio veste grafica

--- js/main.js
+++ js/main.js
@@ -8,14 +8,45 @@ var app = new Vue({
     methods: {
         sendSearch: function(inputSearch) {
             if (inputSearch != '') {
+                this.movieList = [];
+                // CHIAMATA AXIOS FILM
                 axios
-                    .get('https://api.themoviedb.org/3/search/movie?api_key=7fae8a8bc6eec8f4928cbdfba92b7045&language=it-IT&query=' + inputSearch)
+                    .get('https://api.themoviedb.org/3/search/movie', {
+                        params: {
+                            api_key: '7fae8a8bc6eec8f4928cbdfba92b7045',
+                            language: 'it-IT',
+                            query: this.inputSearch
+                        }
+                    })
                     .then((movieSearchList => {
                         movieSearchList.data.results.forEach(movie => {
-                            movie.vote_average = Math.ceil(((movie.vote_average * 5) / 10));
+                            movie.vote_average = Math.ceil(movie.vote_average / 2);
+                            this.movieList.push(movie);
                         });
-                        this.movieList = movieSearchList.data.results;
                     }))
+
+
+                // CHIAMATA AXIOS SERIE TV
+                axios
+                    .get('https://api.themoviedb.org/3/search/tv', {
+                        params: {
+                            api_key: '7fae8a8bc6eec8f4928cbdfba92b7045',
+                            language: 'it-IT',
+                            query: this.inputSearch
+                        }
+                    })
+                    .then((seriesSearchList => {
+                        seriesSearchList.data.results.forEach(series => {
+                            series.vote_average = Math.ceil(series.vote_average / 2);
+                            series.title = series.name;
+                            series.original_title = series.original_name;
+                            delete series['name'];
+                            delete series['original_name'];
+                            this.movieList.push(series);
+
+                        });
+                    }));
+
             } else {
                 this.movieList = [];
             }
