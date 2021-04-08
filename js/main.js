ba4ddb2643d07f3859f1e3c752af8ae48936ca44
Roberto <r.attanasio90@gmail.com>
2021-04-08 13:35:06 +0200

    Aggiunta filtro se si clicca su "film" o "serie tv"

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: '#boolflix',
     data: {
         inputSearch: '',
-        movieList: []
+        movieList: [],
     },
 
     methods: {
@@ -21,6 +21,7 @@ var app = new Vue({
                     .then((movieSearchList => {
                         movieSearchList.data.results.forEach(movie => {
                             movie.vote_average = Math.ceil(movie.vote_average / 2);
+                            movie.contentType = 'Film';
                             this.movieList.push(movie);
                         });
                     }))
@@ -42,6 +43,8 @@ var app = new Vue({
                             series.original_title = series.original_name;
                             delete series['name'];
                             delete series['original_name'];
+                            series.contentType = 'Serie TV';
+
                             this.movieList.push(series);
 
                         });
@@ -50,6 +53,16 @@ var app = new Vue({
             } else {
                 this.movieList = [];
             }
+        },
+
+        filterList: function(filterType) {
+            if (filterType == 'Film') {
+                this.movieList = this.movieList.filter(item => item.contentType == "Film");
+            } else if (filterType == 'Serie TV') {
+                this.movieList = this.movieList.filter(item => item.contentType == "Serie TV");
+
+            };
+
         }
     }
 });
